a39f3c32554870017d76cfc54a31a49e96a66fdd
abap34 <abap0002@gmail.com>
2024-09-05 23:54:57 +0900

    Education と Works 追加

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import About from './components/About';
+import Works from './components/Works';
+import Education from './components/Education';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="bg-white text-gray-800 font-mono min-w-full">
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/Education" element={<Education />} />
+          <Route path="/Works" element={<Works />} />
+        </Routes>
+
+      </div>
+
+      <Footer />
+
+    </Router>
   );
 }
 
