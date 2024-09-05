d716dca0630ee809aae9f745bfea975d62eef33b
abap34 <abap0002@gmail.com>
2024-09-05 23:51:56 +0900

    Header のコンポーネント実装

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom'
+
+export default function Header() {
+  return (
+    <header className="border-b border-gray-200 py-4">
+      <div className="container mx-auto px-4">
+        <nav className="flex justify-between items-center">
+          <Link to="/" className="text-2xl font-bold">
+            <span className="text-blue-600">abap34</span>.com
+          </Link>
+          <div className="space-x-4">
+            <Link to="/blog" className="hover:text-blue-600">
+              Blog
+            </Link>
+            <Link to="/" className="hover:text-blue-600">
+              About
+            </Link>
+            <Link to="/education" className="hover:text-blue-600">
+              Education
+            </Link>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+}
