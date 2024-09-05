625da47dfe205d1bcd9b215461f9e4487d1564f8
abap34 <abap0002@gmail.com>
2024-09-05 23:52:05 +0900

    Footer のコンポーネント実装

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+      <footer className="bg-gray-100 border-t border-gray-200 mt-16 py-8">
+        <div className="container mx-auto px-4 text-center">
+          <p>© 2024 abap34. All rights reserved.</p>
+        </div>
+      </footer>
+    )
+  }
+  
