54a4034d9ec20ed6212a68d2b04563f92f430a76
abap34 <abap0002@gmail.com>
2024-09-07 05:33:29 +0900

    ナビ一旦削除

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,5 @@
 
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 
 import { FaMoon, FaSun } from 'react-icons/fa';
 
@@ -21,31 +20,16 @@ export default function Header() {
   return (
     <header className="border-b border-gray-200 py-4 dark:border-gray-800">
       <div className="container mx-auto px-4">
-        <nav className="flex justify-between items-center">
 
-          <div>
-            <button onClick={() => setIsDark(!isDark)}>
-              {isDark ? <FaSun /> : <FaMoon />}
-            </button>
-          </div>
-
+        <div>
+          <button onClick={() => setIsDark(!isDark)}>
+            {isDark ? <FaSun /> : <FaMoon />}
+          </button>
+        </div>
 
 
-          <Link to="/" className="text-2xl font-bold">
-            <span className="text-blue-600">abap34</span>.com
-          </Link>
-          <div className="space-x-4">
-            <Link to="/blog" className="hover:text-blue-600">
-              Blog
-            </Link>
-            <Link to="/" className="hover:text-blue-600">
-              About
-            </Link>
-            <Link to="/education" className="hover:text-blue-600">
-              Education
-            </Link>
-          </div>
-        </nav>
+
+
       </div>
     </header>
   );
