2d1fc6038e341476aa65fcec96a29bb971d2b303
abap34 <abap0002@gmail.com>
2024-09-08 02:24:46 +0900

    デザイン調整

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,8 @@ import { Book, Code, Cpu, GraduationCap, Lightbulb } from "lucide-react"
 
 export default function About() {
     return (
-        <div className="space-y-8 p-6 rounded-lg">
+        <div className="space-y-8 p-6 m-4">
+            <h1 className="text-4xl font-bold text-blue-600 dark:text-blue-400 dark:text-opacity-80 dark:font-bold"> About Me </h1>
             <section className="space-y-4">
                 <div className="flex items-center space-x-2 text-xl font-semibold">
                     <GraduationCap className="w-6 h-6 text-blue-500" />
