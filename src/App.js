d81265dbdf72926b3f58ab2aee00992a322eab2b
abap34 <abap0002@gmail.com>
2024-09-08 02:25:08 +0900

    ブログはリダイレクトするように変更

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import TopPage from './components/TopPage';
 import Education from './components/Education';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import TopPage from './components/TopPage';
 import Works from './components/Works';
+
+const RedirectToBlog = () => {
+  window.location.href = "https://abap34.com/posts.html";
+  return null; 
+};
 
 function App() {
   return (
@@ -14,8 +19,12 @@ function App() {
 
         <Routes>
           <Route path="/" element={<TopPage />} />
-          <Route path="/Education" element={<Education />} />
-          <Route path="/Works" element={<Works />} />
+          <Route path="/education" element={<Education />} />
+          <Route path="/works" element={<Works title="Projects" path="/works/works.yaml" defaultVisibleCount={3} />} />
+          <Route path="/articles" element={<Works title="Articles" path="/works/articles.yaml" defaultVisibleCount={2} />} />
+          
+          {/* redirect to abap34.com/posts.html */}
+          <Route path="/blog" element={<RedirectToBlog />} />
         </Routes>
 
 
