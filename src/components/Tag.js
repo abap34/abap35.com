d6f1d89405b185665ca033e686e07e1d8250293d
abap34 <abap0002@gmail.com>
2024-09-22 20:43:27 +0900

    複数タグ検索に対応

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -2,10 +2,16 @@ import { FaTag } from 'react-icons/fa';
 
 
 export default function Tag({ name, label }) {
+    const handleClick = () => {
+        const params = new URLSearchParams(window.location.search);
+        params.append('tag', name);
+        window.location.href = `/search?${params.toString()}`;
+    }
+
     return (
         <span className="bg-gray-200 dark:bg-gray-700 text-gray-600 dark:text-gray-400 px-2 py-1 rounded-lg space-x-1 word-break-all truncate">
             <FaTag className="inline-block mr-1" />
-            <a href={`/search?tag=${name}`} className="hover:text-blue-600 transition duration-300">{label}</a>
+            <span onClick={handleClick}>{label}</span>
         </span>
     );
 }
