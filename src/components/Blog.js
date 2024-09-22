1b52f7d0c365610ce19ac997fce5592b0a409a98
abap34 <abap0002@gmail.com>
2024-09-22 17:31:46 +0900

    タグの表示調整

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -116,9 +116,9 @@ export default function Blog() {
                     <BlogTimeline posts={posts} />
                 </div>
 
-                <div className="sticky top-24 overflow-y-scroll hidden lg:block space-y-8 max-h-[80vh] border-gray-200 rounded-lg dark:border-gray-700 border px-8 py-4">
+                <div className="sticky top-24 overflow-y-scroll hidden lg:block space-y-8 max-h-[80vh] border-gray-200 rounded-lg px-8 py-4">
                     <h2 className="text-2xl font-bold mb-4 "> Tags </h2>
-                    <div className="gap-2 flex flex-wrap">
+                    <div className="gap-2 flex flex-wrap dark:border-gray-700 border p-2 rounded-lg">
                         {allTags.map(([tag, count], index) => (
                             <Tag key={index} name={tag} label={`${tag} (${count})`} />
                         ))}
